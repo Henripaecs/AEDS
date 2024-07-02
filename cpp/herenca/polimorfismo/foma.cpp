54012adf6a595afe0bc2b4306b61fae7f54bc626
Henrique Paes de Carvalho <163349410+Henripaecs@users.noreply.github.com>
2024-07-02 11:46:50 -0300

    Update foma.cpp

--- cpp/herenca/polimorfismo/foma.cpp
+++ cpp/herenca/polimorfismo/foma.cpp
@@ -7,10 +7,11 @@ public:
     virtual double perimetro()const = 0;
     virtual double area()const = 0;
     virtual void imprimir()const = 0;
+
     static void mostrar_forma(const forma * forma){
         forma->imprimir();
-        cout << "perimetro" << forma->perimetro()<< endl;
-        cout << "area" << forma->area() << endl;
+        cout << "perimetro " << forma->perimetro()<< endl;
+        cout << "area " << forma->area() << endl;
     }
 };
 class retangulo : public forma{
@@ -18,22 +19,43 @@ private:
     double base;
     double altura;
 public:
-    retangulo(double b, double h): base(b), altura(h){
+    retangulo(double b, double h){
         base = b;
         altura = h;
     }
+
         double perimetro()const override{
             return 2*(base+altura);
         }
         double area()const override{
             return base*altura;
         }
-        void imprimir(){
-            cout << "Lado" << base << "altura" << altura << endl;
+        void imprimir()const override{
+            cout << "Lado " << base << " altura " << altura << endl;
         }
+
+};
+class circulo : public forma{
+private:
+    double raio;
+public:
+    circulo(double r): raio(r){}
+
+        double perimetro()const override{
+            return 2*(3,14*raio);
+        }
+        double area()const override{
+            return 3,14*(raio*raio);
+        }
+        void imprimir()const override{
+            cout << "Raio " << raio  << endl;
+        }
+
 };
 int main(){
     retangulo r(5,3);
-    imprimir();
-    r.mostrar_forma();
+    forma::mostrar_forma(&r);
+
+    circulo c(4);
+    forma::mostrar_forma(&c);
 }
