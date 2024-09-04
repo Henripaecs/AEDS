2ac554219a3e3b488aa6b94b2b5f3cfb12b4d3bf
Henrique Paes de Carvalho <163349410+Henripaecs@users.noreply.github.com>
2024-09-04 08:43:02 -0300

    Update 2024-2_aed1_ed04.c

--- 2024-2_aed1_ed04.c
+++ 2024-2_aed1_ed04.c
@@ -112,25 +112,140 @@ Digite uma cadeia de caracter: AaKkLmM0*Nx
 2 caracteres sao minusculas e menores que M
 E 3 caracteres sao maiusculos e menores que M*/
 
-void exercicio_0416() {
+void exec_416_contar(char c[], int contm, int contM){//funcao para o 416
+    for(int i = 0; i < strlen(c); i++){
+        if(c[i] <= 'z' && c[i] > 'm'){
+            contm++;
+        }
+        if(c[i] <= 'Z' && c[i] > 'M'){
+            contM++;
+        }
+    }
+    printf("%d caracteres sao minusculas e maiores que M\nE %d caracteres sao maiusculos e maiores que M\n", contm, contM);
+}
 
-}/**/
+void exercicio_0416() {
+    char caracter[100];
+    int contm = 0, contM = 0;
+
+    printf("Digite uma cadeia de caracter: ");
+    fgets(caracter, 100, stdin);
+    caracter[strcspn(caracter,"\n")] = '\0';
+
+    exec_416_contar(caracter, contm,contM);
+}/*Escolha um exercicio (1 a 12) sendo 11 e 12 os extras
+                E 0 para sair:
+6
+Digite uma cadeia de caracter: AaKkLmM0*Nx
+1 caracteres sao minusculas e maiores que M
+E 1 caracteres sao maiusculos e maiores que M*/
+
+void exec_417_contar(char c[], int cont){//funcao para o 417
+    for(int i = 0; i < strlen(c); i++){
+        if(isdigit(c[i])){
+            int num = c[i] - '0';
+            if(num % 2 != 0){
+                cont++;
+            }
+        }
+    }
+    printf("Ah %d caracteres impares\n", cont);
+}
 
 void exercicio_0417() {
+    char caracter[100];
+    int cont = 0;
 
-}/**/
+    printf("Digite uma cadeia de caracters: ");
+    fgets(caracter, 100, stdin);
+    caracter[strcspn(caracter,"\n")] = '\0';
+
+    exec_417_contar(caracter, cont);
+}/*Escolha um exercicio (1 a 12) sendo 11 e 12 os extras
+                E 0 para sair:
+7              
+Digite uma cadeia de caracters:  A1b2C3d4E5f6
+Ah 3 caracteres impares*/
+
+void exec_418_contar(char c[], int cont){//funcao para o 418
+    for(int i = 0; i < strlen(c); i++){
+        if(!(isdigit(c[i]))){
+            if(!(c[i] >= 'a' && c[i] <='z' || c[i] >= 'A' && c[i] <= 'Z')){  
+                printf("%c", c[i]);
+            }
+        }
+    }
+    printf("\n");
+}
 
 void exercicio_0418() {
+    char caracter[100];
+    int cont = 0;
 
+    printf("Digite uma cadeia de caracters: ");
+    fgets(caracter, 100, stdin);
+    caracter[strcspn(caracter,"\n")] = '\0';
 
-}/**/
+    exec_418_contar(caracter, cont);
+
+}/*Escolha um exercicio (1 a 12) sendo 11 e 12 os extras
+                E 0 para sair:
+8
+Digite uma cadeia de caracters: (A1b2+C3d4)*E5f6
+(+)**/
+
+void exec_419_contar(char c[], int cont){//funcao para o 419
+    for(int i = 0; i < strlen(c); i++){
+        if((isalnum(c[i]))){
+            printf("%c", c[i]);
+        }
+    }
+    printf("\n");
+}
 
 void exercicio_0419() {
+    char caracter[100];
+    int cont = 0;
 
-}/**/
+    printf("Digite uma cadeia de caracters: ");
+    fgets(caracter, 100, stdin);
+    caracter[strcspn(caracter,"\n")] = '\0';
+
+    exec_419_contar(caracter, cont);
+}/*Escolha um exercicio (1 a 12) sendo 11 e 12 os extras
+                E 0 para sair:
+9
+Digite uma cadeia de caracters:  (A1b2+C3d4)*E5f6
+A1b2C3d4E5f6*/
+
+int exec_420_contar(char c[]){//funcao para o 420
+    int cont = 0;
+    for(int i = 0; i < strlen(c); i++){
+        if(isalnum(c[i])){
+            cont++;
+        }
+    }
+    return cont;
+}
 
 void exercicio_0420() {
+    char caracter[100];
+    int  n, total = 0;
+    int cont;
 
+    printf("Quantas cadeias de caracteres deseja digitar: ");
+    scanf("%d", &n); getchar();
+    printf("Digite uma cadeia de caracters: ");
+    for(int i = 0; i < n; i++){
+        fgets(caracter, 100, stdin);
+        caracter[strcspn(caracter,"\n")] = '\0';
+
+        int cont = exec_420_contar(caracter);   
+        printf("\nA cadeia %s possui %d alfanumericos\n", caracter, cont);
+    }
+    total += cont;
+    printf("O total de caracteres alfanumericos eh %d\n", total);
+    
 
 }/**/
 
